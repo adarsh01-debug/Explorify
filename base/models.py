59165b0ea48d80efe2d2b7623c1fab1167af6419
adarsh01-debug <adarsh.official2299@gmail.com>
2021-11-08 15:13:51 +0530

    u

--- base/models.py
+++ base/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
