b34242d6936a2a20638fb1e54abfa450e932b3e1
adarsh01-debug <adarsh.official2299@gmail.com>
2021-11-08 15:00:15 +0530

    u

--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'name'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 
